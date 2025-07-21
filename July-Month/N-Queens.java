53d9880022bdc9114c6cd1ddd684b2b88a330760
Vishwa V <vvishwa2526@gmail.com>
2025-07-21 19:54:33 +0530

    Update N-Queens.java

--- July-Month/N-Queens.java
+++ July-Month/N-Queens.java
@@ -1,4 +1,4 @@
- N-Queens
+N-Queens
 The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
 Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
 Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
@@ -16,6 +16,7 @@ Constraints:
 1 <= n <= 9
 
 Solution
+ 
 class Solution {
     private void helper(int row,char[][] dp,boolean[] colArr,boolean[] upArr,boolean[] downArr,List<List<String>> result,int N){
         if(row==N){
