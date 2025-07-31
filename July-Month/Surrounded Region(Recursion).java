7045f4d709c5a6a4b129a91a36df598c18db5d29
Vishwa V <vvishwa2526@gmail.com>
2025-07-31 13:01:30 +0530

    Create Surrounded Region(Recursion).java

---
+++ July-Month/Surrounded Region(Recursion).java
@@ -0,0 +1,66 @@
+Surrounded Regions
+You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+
+Connect: A cell is connected to adjacent cells horizontally or vertically.
+Region: To form a region connect every 'O' cell.
+Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+
+ 
+
+Example 1:
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+
+Explanation:
+In the above diagram, the bottom region is not captured because it is on the edge of the board and cannot be surrounded.
+
+Example 2:
+Input: board = [["X"]]
+Output: [["X"]]
+
+
+Constraints:
+m == board.length
+n == board[i].length
+1 <= m, n <= 200
+board[i][j] is 'X' or 'O'.
+
+Solution
+
+  class Solution {
+    public void dfs(int i, int j, char[][] board) {
+if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'T';
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            dfs(x, y, board);
+        }
+    }
+    
+    public void solve(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i++) {
+            if (board[i][0] == 'O') dfs(i, 0, board);
+            if (board[i][col - 1] == 'O') dfs(i, col - 1, board);
+        }
+        for (int j = 0; j < col; j++) {
+            if (board[0][j] == 'O') dfs(0, j, board);
+            if (board[row - 1][j] == 'O') dfs(row - 1, j, board);
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+}
